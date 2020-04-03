b02e225b14cf54f10803137f22eab7f3d5615263
roman <roman.mueller@swisslife.ch>
2020-04-03 22:10:35 +0200

    rgb led shows status during wifi connection

--- wfh_indicator_esp8266.ino
+++ wfh_indicator_esp8266.ino
@@ -1,10 +1,11 @@
-/******************************************************************************************************************
-*                                                                  
-*                                                                  
-*                                                                  
-* Based on the example sketch on github:                                                                 
-* https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/EchoBot/EchoBot.ino
-*******************************************************************************************************************/
+/************************************************************************************************************************
+
+
+
+  Based on the example sketches on github:
+  https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/EchoBot/EchoBot.ino
+  https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/ChatAction/ChatAction.ino
+*************************************************************************************************************************/
 #include "secrets.h"
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
@@ -31,19 +32,19 @@ const int MIN_ANALOG = 1023;
 const int MAX_ANALOG = 0;
 const int MIN_COLOR  = 0;
 const int MAX_COLOR  = 255;
-  
+
 void setup() {
-  
+
   Serial.begin(115200);
 
   pinMode(R_PIN, OUTPUT);
   pinMode(G_PIN, OUTPUT);
   pinMode(B_PIN, OUTPUT);
-  
-  analogWrite(R_PIN, 1023);  
+
+  analogWrite(R_PIN, 1023);
   analogWrite(G_PIN, 0);
   analogWrite(B_PIN, 0);
-  
+
   // Set WiFi to station mode and disconnect from an AP if it was Previously
   // connected
   WiFi.mode(WIFI_STA);
@@ -57,32 +58,82 @@ void setup() {
   Serial.println(ssid);
   WiFi.begin(ssid, password);
 
+  analogWrite(R_PIN, 0);
+
+  bool toggle = true;
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
+
+    if (toggle == true) {
+      analogWrite(B_PIN, 1023);
+    }
+    else {
+      analogWrite(B_PIN, 0);
+    }
+    toggle = !toggle;
+
     delay(500);
   }
 
-  analogWrite(R_PIN, 0);  
+  analogWrite(B_PIN, 0);
   analogWrite(G_PIN, 1023);
-  
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  delay(500);
+  analogWrite(G_PIN, 0);
 }
 
 void loop() {
   if (millis() > Bot_lasttime + Bot_mtbs)  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-    while(numNewMessages) {
+    while (numNewMessages) {
       Serial.println("got response");
-      for (int i=0; i<numNewMessages; i++) {
-        bot.sendMessage(bot.messages[i].chat_id, bot.messages[i].text, "");
-      }
+      handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
 
     Bot_lasttime = millis();
+  }
+}
+
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    String text = bot.messages[i].text;
+
+    String from_name = bot.messages[i].from_name;
+    if (from_name == "") from_name = "Guest";
+
+    if (text == "/send_test_action") {
+      bot.sendChatAction(chat_id, "typing");
+      delay(4000);
+      bot.sendMessage(chat_id, "Did you see the action message?");
+
+      // You can't use own message, just choose from one of bellow
+
+      //typing for text messages
+      //upload_photo for photos
+      //record_video or upload_video for videos
+      //record_audio or upload_audio for audio files
+      //upload_document for general files
+      //find_location for location data
+
+      //more info here - https://core.telegram.org/bots/api#sendchataction
+    }
+
+    if (text == "/start") {
+      String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
+      welcome += "This is Chat Action Bot example.\n\n";
+      welcome += "/send_test_action : to send test chat action message\n";
+      bot.sendMessage(chat_id, welcome);
+    }
   }
 }
