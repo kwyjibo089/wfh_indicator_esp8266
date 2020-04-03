01e3645cab38a70c10230471e16e3acbefc28e04
roman <roman.mueller@swisslife.ch>
2020-04-03 21:17:36 +0200

    initial chatbot

---
+++ wfh_indicator_esp8266.ino
@@ -0,0 +1,67 @@
+/*******************************************************************
+*  An example of bot that echos back any messages received         *
+*                                                                  *
+*  written by Giacarlo Bacchio (Gianbacchio on Github)             *
+*  adapted by Brian Lough                                          *
+*******************************************************************/
+#include "secrets.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+
+// Initialize Wifi connection to the router
+char ssid[] = SECRET_SSID;     // your network SSID (name)
+char password[] = SECRET_PASS; // your network key
+
+// Initialize Telegram BOT
+#define BOTtoken SECRET_BOT_TOKEN  // your Bot Token (Get from Botfather)
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+int Bot_mtbs = 1000; //mean time between scan messages
+long Bot_lasttime;   //last time messages' scan has been done
+
+void setup() {
+  Serial.begin(115200);
+  
+  client.setInsecure();  // TLS problem
+  
+  // Set WiFi to station mode and disconnect from an AP if it was Previously
+  // connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if (millis() > Bot_lasttime + Bot_mtbs)  {
+    Serial.println("scanning for new messages...");
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      for (int i=0; i<numNewMessages; i++) {
+        bot.sendMessage(bot.messages[i].chat_id, bot.messages[i].text, "");
+      }
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    Bot_lasttime = millis();
+  }
+}
