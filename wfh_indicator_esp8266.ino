a5ab28be40d279ab54655f6a8ac42ea85413b9a5
roman <roman.mueller@swisslife.ch>
2020-04-04 21:11:28 +0200

    finished initial programming and added jpg

--- wfh_indicator_esp8266.ino
+++ wfh_indicator_esp8266.ino
@@ -1,6 +1,9 @@
 /************************************************************************************************************************
+  Work from home indicator.
+  Using a NodeMCU with two RGB leds to indicate status on a picture frame.
 
-
+  Original idea by Steve Forde (@cairn4)
+  https://twitter.com/cairn4/status/1245539977993355265
 
   Based on the example sketches on github:
   https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP8266/EchoBot/EchoBot.ino
@@ -24,6 +27,7 @@ UniversalTelegramBot bot(BOTtoken, client);
 int Bot_mtbs = 1000; //mean time between scan messages
 long Bot_lasttime;   //last time messages' scan has been done
 
+// the two rgb leds
 const int  D1_RED = D8;
 const int  D1_GREEN = D7;
 const int  D1_BLUE = D6;
@@ -36,6 +40,11 @@ const int MIN_ANALOG = 1023;
 const int MAX_ANALOG = 0;
 const int MIN_COLOR  = 0;
 const int MAX_COLOR  = 255;
+
+// for the rainbow effect
+int r = 0;
+int g = 0;
+int b = 0;
 
 void setup() {
 
@@ -110,9 +119,14 @@ void loop() {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
     while (numNewMessages) {
+      analogWrite(D1_BLUE, 1023);
+      analogWrite(D2_BLUE, 1023);
+      delay(250);
       Serial.println("got response");
+      analogWrite(D1_BLUE, 0);
+      analogWrite(D2_BLUE, 0);      
       handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);      
     }
 
     Bot_lasttime = millis();
@@ -130,48 +144,92 @@ void handleNewMessages(int numNewMessages) {
     String from_name = bot.messages[i].from_name;
     if (from_name == "") from_name = "Guest";
 
-    if (text == "/send_test_action") {
-      bot.sendChatAction(chat_id, "typing");
-      delay(4000);
-      bot.sendMessage(chat_id, "Did you see the action message?");
-
-      // You can't use own message, just choose from one of bellow
-
-      //typing for text messages
-      //upload_photo for photos
-      //record_video or upload_video for videos
-      //record_audio or upload_audio for audio files
-      //upload_document for general files
-      //find_location for location data
-
-      //more info here - https://core.telegram.org/bots/api#sendchataction
-    }
-
-    else if (text == "/start") {
+    if (text == "/hello" || text == "/start") {
       String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
       welcome += "This is Chat Action Bot example.\n\n";
       welcome += "/send_test_action : to send test chat action message\n";
       bot.sendMessage(chat_id, welcome);
     }
 
-    else if (text == "/red") {
+    else if (text == "/meetingon" || text == "/1on") {
       String msg = "Received message " + text + " from " + from_name + ".\n";
       bot.sendChatAction(chat_id, "typing");
       bot.sendMessage(chat_id, msg);
       delay(500);
       analogWrite(D1_RED, 1023);
-      delay(4000);
-      analogWrite(D1_RED, 0);
     }
 
-    else if (text == "/blue") {
+    else if (text == "/meetingoff" || text == "/1off") {
       String msg = "Received message " + text + " from " + from_name + ".\n";
       bot.sendChatAction(chat_id, "typing");
       bot.sendMessage(chat_id, msg);
       delay(500);
-      analogWrite(D2_BLUE, 1023);
-      delay(4000);
+      analogWrite(D1_RED, 0);
+    }
+
+    else if (text == "/headphoneson" || text == "/2on") {
+      String msg = "Received message " + text + " from " + from_name + ".\n";
+      bot.sendChatAction(chat_id, "typing");
+      bot.sendMessage(chat_id, msg);
+      delay(500);
+      analogWrite(D2_GREEN, 1023);
+    }
+
+    else if (text == "/headphonesoff" || text == "/2off") {
+      String msg = "Received message " + text + " from " + from_name + ".\n";
+      bot.sendChatAction(chat_id, "typing");
+      bot.sendMessage(chat_id, msg);
+      delay(500);
+      analogWrite(D2_GREEN, 0);
+    }
+
+    
+    else if (text == "/rainbow") {
+      String msg = "Received message " + text + " from " + from_name + ".\n";
+      bot.sendChatAction(chat_id, "typing");
+      bot.sendMessage(chat_id, msg);
+      delay(500);
+
+      setColor(1023, 0, 0);    // red
+      setColor(0, 1023, 0);    // green
+      setColor(0, 0, 1023);    // blue
+      setColor(1023, 1023, 0);  // yellow
+      setColor(320, 0, 320);    // purple
+      setColor(0, 1023, 1023);  // aqua
+    }
+
+    else {
+      String msg = "Received message " + text + " from " + from_name + ".\n";
+      msg += "Don\'t know what to do...\n";
+      bot.sendChatAction(chat_id, "typing");
+      bot.sendMessage(chat_id, msg);
+      delay(500);
       analogWrite(D2_BLUE, 0);
     }
+  }  
+}
+
+void setColor(int red, int green, int blue) {
+  while ( r != red || g != green || b != blue ) {
+    if ( r < red ) r += 1;
+    if ( r > red ) r -= 1;
+
+    if ( g < green ) g += 1;
+    if ( g > green ) g -= 1;
+
+    if ( b < blue ) b += 1;
+    if ( b > blue ) b -= 1;
+
+    _setColor();
+    delay(10);
   }
+}
+
+void _setColor() {
+  analogWrite(D1_RED, r);
+  analogWrite(D2_RED, r);
+  analogWrite(D1_GREEN, g);
+  analogWrite(D2_GREEN, g);
+  analogWrite(D1_BLUE, b); 
+  analogWrite(D2_BLUE, b); 
 }
