cb2e0337adca7bcfae0ca8e09ea3aadc4138e62c
roman <roman.mueller@swisslife.ch>
2020-04-05 15:16:16 +0200

    deleted build folder

--- Arduino/build/core/cont.S.d
+++
@@ -1,2 +0,0 @@
-c:\Users\roman\OneDrive\Documents\Arduino\wfh_indicator_esp8266\Arduino\build\core\cont.S.o: \
- C:\Users\roman\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3\cores\esp8266\cont.S
--- Arduino/build/core/umm_malloc/umm_integrity.c.d
+++
@@ -1,2 +0,0 @@
-c:\Users\roman\OneDrive\Documents\Arduino\wfh_indicator_esp8266\Arduino\build\core\umm_malloc\umm_integrity.c.o: \
- C:\Users\roman\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3\cores\esp8266\umm_malloc\umm_integrity.c
